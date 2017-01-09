1cea0798d6014d44223f472619581784dd116c2e
Bill Reed <billnreed@gmail.com>
2017-01-09 00:17:48 -0500

    fix formatting

--- app/scripts/modules/states/check-win-state.js
+++ app/scripts/modules/states/check-win-state.js
@@ -9,12 +9,12 @@ class CheckWinState {
 
     onEnter() {
         const remainingTileCount = Tiles.getRemainingCount();
+        
         if (remainingTileCount == 0) {
             App.transitionTo(States.WIN);
         } else {
             App.transitionTo(States.RESET_DICE);
         }
-
     }
 
     onExit() {}
